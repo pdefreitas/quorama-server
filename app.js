9bd361398e3efcd7e294ac6536dbb6eaeb411dd5
pdefreitas <pdefreitas@users.noreply.github.com>
2017-11-24 13:27:24 +0000

    test

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', function(req, res, next) {
-  console.log(req);  
+app.use(function(req, res, next) {
+  console.log(req); 
+  next()
 });
 
 app.use('/', index);
