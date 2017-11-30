9000a44c39c8a8ef338ab8b4e7299632edfbfbec
pdefreitas <pdefreitas@users.noreply.github.com>
2017-11-30 19:14:52 +0000

    progress

--- plugins/ci/jenkins.js
+++ plugins/ci/jenkins.js
@@ -16,14 +16,14 @@ app.post('/artifacts/add', multipartMiddleware, function(req, res, next) {
 
   for (var key in req.files) {
     if (req.files.hasOwnProperty(key)) {           
-        console.log(key, req.files[key]);
-        var file = req.files[key];
-        var file_name = file.originalFilename;
-        var file_path = file.path;
+        console.log(key, req.files[key])
+        var file = req.files[key]
+        var file_name = file.originalFilename
+        var file_path = file.path
     }
   }
 
-  res.json(200, {status: true})
+  res.status(200).json({status: true})
 });
 
-module.exports = app;
+module.exports = app
